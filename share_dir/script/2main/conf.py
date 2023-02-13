6c783e270dd0a0f489143219fe5a568300e93beb
wenze.song <wenze.song@okg.com>
2023-02-13 16:00:07 +0800

    a

--- share_dir/script/2main/conf.py
+++ share_dir/script/2main/conf.py
@@ -1,17 +1,17 @@
-DRIVER_PATH = '/Users/songwenze/Downloads/geckodriver'
-FIREFOX_PROFILE_PATH = '/opt/data/shm'
-MYSQL_CONF = {
-    'host': "localhost",
-    'user': "root",
-    'password': "123456",
-    'database': "compass"
-}
-
-# DRIVER_PATH = '/usr/local/bin/geckodriver'
-# FIREFOX_PROFILE_PATH = '/dev/shm'
+# DRIVER_PATH = '/Users/songwenze/Downloads/geckodriver'
+# FIREFOX_PROFILE_PATH = '/opt/data/shm'
 # MYSQL_CONF = {
-#     'host': "10.13.76.137",
+#     'host': "localhost",
 #     'user': "root",
 #     'password': "123456",
 #     'database': "compass"
 # }
+
+DRIVER_PATH = '/usr/local/bin/geckodriver'
+FIREFOX_PROFILE_PATH = '/dev/shm'
+MYSQL_CONF = {
+    'host': "10.13.76.137",
+    'user': "root",
+    'password': "123456",
+    'database': "compass"
+}
